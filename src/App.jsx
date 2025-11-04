be43619c3be1724d39694d6ec499d225f03c0954
Sojib Islam <sojibislam6505@gmail.com>
2025-11-05 00:54:51 +0600

    initialize my project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Blogs from './component/blogs/Blogs'
+import Header from './component/header/Header'
+function App() {
+  return (
+    <>
+     <Header></Header>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+    </>
+  )
+}
+
+
